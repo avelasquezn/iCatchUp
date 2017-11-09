4bb2f0a114cae2e479b9dbc7ca356050d823ea44
Angel Velasquez <angel@xendacentral.com>
2017-11-09 16:58:57 -0500

    Added

---
+++ iCatchUp/Model/SettingsStore.swift
@@ -0,0 +1,22 @@
+//
+//  SettingsStore.swift
+//  iCatchUp
+//
+//  Created by Operador on 11/9/17.
+//  Copyright © 2017 UPC. All rights reserved.
+//
+
+import Foundation
+
+class SettingsStore {
+    var newsApiKey: String {
+        get {
+            let path = Bundle.main
+                .path(forResource: "Keys", ofType: "plist")
+            let keys = NSDictionary(contentsOfFile: path!)
+                as? [String: AnyObject]
+            return keys!["NewsApiKey"] as! String
+        }
+    }
+    
+}
